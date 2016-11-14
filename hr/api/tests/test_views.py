653665be65db3d6cb39dddd3dd12f7cf77217014
Douglas Anderson <hockeybuggy@gmail.com>
2016-11-14 15:17:09 -0500

    Add tests for `PUT`. Missing a utility to create initial records.

--- hr/api/tests/test_views.py
+++ hr/api/tests/test_views.py
@@ -13,6 +13,14 @@ class ExpenseAPIView(TestCase):
             content_type="application/json"
         )
 
+    def put_payload(self, payload):
+        # TODO clean up these test helper methods
+        return self.c.put(
+            reverse("expense-api"),
+            data=payload,
+            content_type="application/json"
+        )
+
     def test_post__simple_create(self):
         payload = {
             "amount": "150.0000",
@@ -28,3 +36,29 @@ class ExpenseAPIView(TestCase):
     def test_post__bad_data(self):
         response = self.post_payload({})
         self.assertEqual(400, response.status_code)
+
+    def test_put__simple(self):
+        # TODO set up previous create record.
+        self.fail("Need to have existing create record in the event log")
+
+        payload = {
+            "amount": "150.0000",
+            "date": "2016-11-14T12:34:56",
+            "name": "pycon ticket",
+            "sequence": 2,
+        }
+        response = self.put_payload(payload)
+
+        self.assertEqual(201, response.status_code)
+
+    def test_put__does_not_have_previous_create(self):
+        payload = {
+            "amount": "150.0000",
+            "date": "2016-11-14T12:34:56",
+            "name": "pycon ticket",
+            "sequence": 2,
+        }
+
+        response = self.put_payload(payload)
+
+        self.assertEqual(201, response.status_code)
