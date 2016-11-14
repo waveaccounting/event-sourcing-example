f6b74ad733444781060361a8f3bd8ce9cac6ebeb
guptamo <mo@mohitgupta.ca>
2016-11-14 14:25:56 -0500

    added extra digits amount field on ExpenseLogAggregate table

--- hr/api/models.py
+++ hr/api/models.py
@@ -6,6 +6,6 @@ class ExpenseEventLog(models.Model):
     guid = models.IntegerField()
 
 class ExpenseLogAggregate(models.Model):
-    amount = models.DecimalField(max_digits = 7, decimal_places = 2)
-    description = models.CharField(max_length = 200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=200)
     date = models.DateTimeField()
