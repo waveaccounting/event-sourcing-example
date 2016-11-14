e9b2f619f9bcb92587312e9988a93020f1623da8
guptamo <mo@mohitgupta.ca>
2016-11-14 13:54:49 -0500

    Removed unnecessary blank = False to modelfields

--- hr/api/models.py
+++ hr/api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class ExpenseEventLog(models.Model):
-    event_data = models.TextField(blank = False)
-    sequence = models.IntegerField(blank = False)
+    event_data = models.TextField()
+    sequence = models.IntegerField()
     guid = models.IntegerField()
