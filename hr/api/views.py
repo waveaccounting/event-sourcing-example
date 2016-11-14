b4c25ef41202ac7bf6f62537540b1c24ea931a3c
Brian Graham <bgraham@waveapps.com>
2016-11-14 13:31:34 -0500

    Rough estimate of what the interfaces can look like

--- hr/api/views.py
+++ hr/api/views.py
@@ -2,19 +2,40 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import status
 
-from api.serializers import ExpenseSerializer
-
+from api.serializers import EventMethod, InvalidEvent, EventlogPreconditionFailure, EventLog, EventFactory, ExpenseValidator
 
 class ExpenseAPIView(APIView):
-    def get(self, request, format=None):
-        pass
+    def dispatch(self, request):
+        if request.method == request.GET:
+            pass # TODO
 
-    def post(self, request, format=None):
-        serialzer = ExpenseSerializer(data=request.data)
-        if serializer.is_valid()
-            serialzer.save()
-            return Response(serialzer.data, status=status.HTTP_201_CREATED)
-        return Response(serialzer.errors, status=status.HTTP_400_BAD_REQUEST)
+        sequence=request.data.sequence
+
+        expenseEventFactory = EventFactory(ExpenseValidator)
+
+        try:
+            event = expenseEventFactory(
+                sequence,
+                request.data,
+                self._getMethod(request.method)
+            )
+        except InvalidEvent:
+            return Response(serialzer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            eventlog.publish(event)
+        except EventlogPreconditionFailure:
+            return Response(serialzer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serialzer.data, status=status.HTTP_201_CREATED)
+
+    def _getMethod():
+        if request.method == request.POST:
+            return EventMethod.CREATE
+        if request.method == request.PUT:
+            return EventMethod.UPDATE
+        if request.method == request.DELETE:
+            return EventMethod.DELETE
 
 
 class MonthlyReportAPIView(APIView):
