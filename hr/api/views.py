535fc56bedd72b03f0a848f5021e0c3ebb299606
Douglas Anderson <hockeybuggy@gmail.com>
2016-11-14 11:03:17 -0500

    Add stubs for the endpoints.

--- hr/api/views.py
+++ hr/api/views.py
@@ -1,4 +1,14 @@
 from rest_framework.views import import APIView
 
-class HrAPIView(APIView):
-    pass
+
+class ExpenseAPIView(APIView):
+    def get(self, request, format=None):
+        pass
+
+    def post(self, request, format=None):
+        pass
+
+
+class MonthlyReportAPIView(APIView):
+    def get(self, request, format=None):
+        pass
