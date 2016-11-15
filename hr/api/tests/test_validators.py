27e1273e521b00afea196091ebe8f89c40c8fe76
Brian Graham <bgraham@waveapps.com>
2016-11-15 09:52:20 -0500

    Because I'm writing tests with my brain now.

--- hr/api/tests/test_validators.py
+++ hr/api/tests/test_validators.py
@@ -8,33 +8,50 @@ class ExpenseValidatorTest(TestCase):
     def setUp(self):
         self.v = ExpenseValidator()
 
-    def test_empty_payload(self, payload):
-        return self.v.validate({})
+    def test_empty_payload(self):
+        self.assertRaises(Exception, self.v.validate, {})
 
     def test_payload_missing_amount(self):
-        return self.v.validate({
+        self.assertRaises(Exception, self.v.validate, {
             "date": "2016-11-14T12:34:56",
             "name": "pycon ticket",
             "sequence": 1,
         })
 
     def test_payload_missing_date(self):
-        return self.v.validate({
+        self.assertRaises(Exception, self.v.validate, {
             "amount": "150.0000",
             "name": "pycon ticket",
             "sequence": 1,
         })
 
     def test_payload_missing_name(self):
-        return self.v.validate({
+        self.assertRaises(Exception, self.v.validate, {
             "amount": "150.0000",
             "date": "2016-11-14T12:34:56",
             "sequence": 1,
         })
 
     def test_payload_missing_sequence(self):
-        return self.v.validate({
+        self.assertRaises(Exception, self.v.validate, {
             "amount": "150.0000",
             "date": "2016-11-14T12:34:56",
             "name": "pycon ticket",
         })
+
+    def test_payload_bad_amount_format(self):
+        self.assertRaises(Exception, self.v.validate, {
+            "amount": "this isn't money",
+            "date": "2016-11-14T12:34:56",
+            "name": "pycon ticket",
+            "sequence": 2,
+        })
+
+    def test_payload_valid_shape(self):
+        valid = self.v.validate({
+            "amount": "150.0000",
+            "date": "2016-11-14T12:34:56",
+            "name": "pycon ticket",
+            "sequence": 2,
+        })
+        self.assertEqual(True, valid)
--- hr/api/validators.py
+++ hr/api/validators.py
@@ -1,15 +1,21 @@
 from event_source.exceptions import InvalidEvent
+import re
 
 
 class ExpenseValidator(object):
-    def validate(data):
+    def validate(self, data):
         if type(data["amount"]) is not str:
-            raise InvalidEvent
+            raise InvalidEvent('Amount is not a string')
+
+        r = re.compile(r'[0-9]+[.][0-9][0-9]')
+        if r.match(data["amount"]) is None:
+            raise InvalidEvent('Amount is not decimal format')
+
         if type(data["date"]) is not str:
-            raise InvalidEvent
+            raise InvalidEvent('Date is not a string')
         if type(data["name"]) is not str:
-            raise InvalidEvent
+            raise InvalidEvent('name is not a string')
         if type(data["sequence"]) is not int:
-            raise InvalidEvent
+            raise InvalidEvent('Sequence is not an int')
 
         return True
