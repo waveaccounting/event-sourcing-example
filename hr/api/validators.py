b2032a5a425d859825d2924490ac73b477148b79
Brian Graham <bgraham@waveapps.com>
2016-11-15 15:04:33 -0500

    Fix validator to have constraints again.

--- hr/api/validators.py
+++ hr/api/validators.py
@@ -4,8 +4,26 @@ import re
 
 class ExpenseValidator(object):
     def validate(self, data):
+        if self._isUnicodeString(data["amount"]):
+            raise InvalidEvent('Amount is not a string')
+
         r = re.compile(r'[0-9]+[.][0-9][0-9]')
         if r.match(data["amount"]) is None:
             raise InvalidEvent('Amount is not decimal format')
 
+        if self._isUnicodeString(data["date"]):
+            raise InvalidEvent('Date is not a string')
+        if self._isUnicodeString(data["name"]):
+            raise InvalidEvent('name is not a string')
+        if self._isUnicodeString(data["sequence"]):
+            raise InvalidEvent('Sequence is not an int')
+
         return True
+
+    def _isUnicodeString(self, string):
+        if string is str:
+            return True
+        if string is unicode:
+            return True
+
+        return False
