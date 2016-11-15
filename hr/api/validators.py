6c9e95dbdfd0b9a30e7803ad5bacdc3e4c8015f9
Brian Graham <bgraham@waveapps.com>
2016-11-15 15:57:47 -0500

    Use six for validation checking

--- hr/api/validators.py
+++ hr/api/validators.py
@@ -1,29 +1,23 @@
-from event_source.exceptions import InvalidEvent
+import six
 import re
+
+from event_source.exceptions import InvalidEvent
 
 
 class ExpenseValidator(object):
     def validate(self, data):
-        if self._isUnicodeString(data["amount"]):
+        if isinstance(data["amount"], six.string_types):
             raise InvalidEvent('Amount is not a string')
 
         r = re.compile(r'[0-9]+[.][0-9][0-9]')
         if r.match(data["amount"]) is None:
             raise InvalidEvent('Amount is not decimal format')
 
-        if self._isUnicodeString(data["date"]):
+        if isinstance(data["date"], six.string_types):
             raise InvalidEvent('Date is not a string')
-        if self._isUnicodeString(data["name"]):
+        if isinstance(data["name"], six.string_types):
             raise InvalidEvent('name is not a string')
-        if self._isUnicodeString(data["sequence"]):
+        if isinstance(data["sequence"], six.integer_types):
             raise InvalidEvent('Sequence is not an int')
 
         return True
-
-    def _isUnicodeString(self, string):
-        if string is str:
-            return True
-        if string is unicode:
-            return True
-
-        return False
